88e57a58f92c8b51a2f697142f77c3ee11a5cc72
jamal-nasser <nasserr.jamal@gmail.com>
2021-06-28 13:20:21 +0200

    initial commit

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,22 @@
 import React from 'react';
-
+import { Route } from 'react-router-dom';
+import { Container } from 'react-bootstrap';
+import Header from './components/Header/Header';
+import Footer from './components/Footer/Footer';
+import HomeScreen from './Screens/HomeScreen';
+import ProductScreen from './Screens/ProductScreen';
 
 const App = () => {
   return (
     <>
-      <h1>Welcome to nerfs</h1>
+      <Header />
+      <main className='py-3'>
+        <Container>
+          <Route exact path='/' component={HomeScreen} />
+          <Route path='/product/:id' component={ProductScreen} />
+        </Container>
+      </main>
+      <Footer />
     </>
   );
 }
